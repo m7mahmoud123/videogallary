5092a64328a0f2a9a2fef27b1139fc24ce4d6471
mahmoud saad mohammed abdelall <121638824+m7mahmoud123@users.noreply.github.com>
2023-07-14 20:00:20 +0300

    Add files via upload

---
+++ video gallary/script.js
@@ -0,0 +1,49 @@
+let nymar= document.getElementsByClassName("two")[0]
+nymar.onclick = function(){
+
+    document.getElementsByClassName("title")[0].innerHTML="<div class='title'><p>Neymar Jr ●King Of Dribbling Skills● 2023 |HD</p></div>"
+    document.getElementsByTagName("iframe")[0].src="https://www.youtube.com/embed/ofIX3daQtSc"
+    
+}
+
+let messi= document.getElementsByClassName("one")[0]
+messi.onclick = function(){
+
+    document.getElementsByTagName("iframe")[0].src="https://www.youtube.com/embed/p693u53Q10U"
+    
+}
+
+
+let salah= document.getElementsByClassName("three")[0]
+salah.onclick = function(){
+
+    document.getElementsByClassName("title")[0].innerHTML="<div class='title'><p>Mohamed Salah 2021/2022 - Speed Show , Skills & Goals - HD</p></div>"
+    document.getElementsByTagName("iframe")[0].src="https://www.youtube.com/embed/ypnFYOURFzo"
+    
+}
+
+
+let Mpappe= document.getElementsByClassName("four")[0]
+Mpappe.onclick = function(){
+
+    document.getElementsByClassName("title")[0].innerHTML="<div class='title'><p>K. Mbappe ● King Of Speed Skills ● 2023 | 1080i 60fps</p></div>"
+    document.getElementsByTagName("iframe")[0].src="https://www.youtube.com/embed/Cw16gPWSFxE"
+    
+}
+
+
+let suarez= document.getElementsByClassName("five")[0]
+suarez.onclick = function(){
+
+    document.getElementsByClassName("title")[0].innerHTML="<div class='title'><p>Luis Suárez 2023 ● Grêmio ► Magic Skills, Goals & Assists | HD</p></div>"
+    document.getElementsByTagName("iframe")[0].src="https://www.youtube.com/embed/lp9XLm5SwPM"
+    
+}
+
+let Ronaldo= document.getElementsByClassName("six")[0]
+Ronaldo.onclick = function(){
+
+    document.getElementsByClassName("title")[0].innerHTML="<div class='title'><p>Cristiano Ronaldo ●King Of Dribbling Skills● HD</p></div>"
+    document.getElementsByTagName("iframe")[0].src="https://www.youtube.com/embed/1c8IMlFodzc"
+    
+}
